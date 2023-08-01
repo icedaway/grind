9d98b5da07861a564eb239087680b3d5dbce47e6
Rudeecho <me19s016@smail.iitm.ac.in>
2023-08-02 00:49:00 +0530

    Solved some basic atcoder problems.

---
+++ abc240a.cpp
@@ -0,0 +1,84 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> oset;
+
+
+#define int long long
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef map<int,int> mii;
+typedef queue<int> qi;
+typedef set<int> si;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef priority_queue<int,std::vector<int>,greater<int>> min_prior;
+typedef priority_queue<int> prior;
+
+#define fori(i,n) for(int i = 0; i < n; i++)
+#define rfori(i,n) for(int i = n; i >=0; i--)
+#define fora(i,a,b) for(int i = a; i <= b; i++)
+#define rfora(i,a,b) for(int i = a; i >= b; i--)
+#define each(a,x) for(auto a: x)
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+#define MOD 1000000007
+#define MOD1 998244353
+#define fill(str,a) memset(str, a, sizeof(str));
+#define INF 1e18
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define lb lower_bound
+#define ub upper_bound
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define pos(v,a) find(all(v),a) - v.begin()
+#define sz(v) (int)v.size()
+#define pm(v) std::cout << v << " ";
+#define pn(v) std::cout << v << "\n";
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define maxv(v) *max_element(all(v))
+#define minv(v) *min_element(all(v))
+#define maxind(v) max_element(all(v)) - v.begin()
+#define minind(v) min_element(all(v)) - v.begin()
+#define uniq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define int_max 2147483647
+#define int_min -2147483647
+#define isthere(v,n) (find(all(v),n)!=v.end())
+#define delv(v,n) v.erase(find(all(v),n))
+
+#ifndef ONLINE_JUDGE
+#include "../judge.h"
+#else
+#define dbg(x)
+void init(void){}
+#endif
+
+
+
+
+void fun(){
+  int a,b;std::cin >> a >> b;
+  if(abs(a - b) == 1 || abs(a - b) == 9){std::cout << "Yes" << "\n";}
+  else{std::cout << "No" << "\n";}
+
+}
+
+
+
+
+int32_t main(){
+  ios:: sync_with_stdio(0),cin.tie(0);cout<<fixed<<setprecision(20);init();int t = 1;
+  // std::cin >> t;
+  while(t--)
+    fun();
+  return 0;
+}
+/*
+*/
